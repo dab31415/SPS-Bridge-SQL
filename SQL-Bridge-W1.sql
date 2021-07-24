d949ef3d875e88f42a6139109b3c1e1c96b2dd50
dab31415 <87675090+dab31415@users.noreply.github.com>
2021-07-24 12:43:50 -0400

    Update SQL-Bridge-W1.sql

--- SQL-Bridge-W1.sql
+++ SQL-Bridge-W1.sql
@@ -5,12 +5,18 @@
 
 -- 1. Which destination in the flights database is the furthest distance away, based on information 
 --     in the flights table. Show the SQL query(s) that support your conclusion
-SELECT dest FROM flights ORDER BY distance DESC LIMIT 1;
+SELECT DISTINCT dest 
+FROM flights 
+WHERE distance = (SELECT MAX(distance) FROM flights);
 --    HNL: Honolulu Intl
 
 -- 2. What are the different numbers of engines in the planes table? For each number of engines, which aircraft have
 --     the most number of seats? Show the SQL statement(s) that support your result.
-SELECT engines, MAX(seats) FROM planes GROUP BY engines ORDER BY engines;
+SELECT DISTINCT engines FROM planes ORDER BY engines;
+SELECT p.tailnum, p.manufacturer, p.model, p.engines, p.seats
+FROM planes p
+INNER JOIN (SELECT engines, MAX(seats) maxseats FROM planes GROUP BY engines) ps ON ps.engines = p.engines And ps.maxseats = p.seats
+ORDER BY p.engines, p.manufacturer, p.model, p.tailnum;
 
 -- 3. Show the total number of flights.
 SELECT COUNT(*) FROM flights;
@@ -30,4 +36,5 @@ SELECT carrier, COUNT(*) FROM flights WHERE distance >= 1000 GROUP BY carrier OR
 
 -- 8. Create a question that (a) uses data from the flights database, and (b) requires aggregation to answer it, and
 --    write down both the question, and the query that answers the question.
-
+--    Show the top 5 airlines with departure delays of 60 minutes or longer.
+SELECT carrier, COUNT(*) FROM flights WHERE dep_delay >= 60 GROUP BY carrier ORDER BY COUNT(*) DESC LIMIT 5;
